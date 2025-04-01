6b10d3a9f036e719817b071a749ee205a1d2aa9c
AhHyeon <dmb09103@gmail.com>
2025-04-01 10:33:28 +0900

    잘못된 과제 삭제

--- week02/script.js
+++
@@ -1,9 +0,0 @@
-function RandomNumber() {
-    const number = [];
-    while (number.length<6){
-        const num = Math.floor(Math.random() * 45) + 1;
-        if (!number.includes(num)) {
-            number.push(num);
-        }
-    }
-}
